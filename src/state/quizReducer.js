702673f2163039c7e55b79235a367cb45e112f54
psi <cylonsido@gmail.com>
2026-09-09 22:21:41 +0800

    manage quiz state with useReducer

--- src/state/quizReducer.js
+++ src/state/quizReducer.js
@@ -1,5 +1,33 @@
-export function quizReducer(state, action) {
+export function quizReducer(state, action, questionsLength) {
   switch(action.type) {
+    case 'ANSWER': {
+      const newAnswers = [...state.answers]
+      newAnswers[state.currentQuestion] = action.payload
+      return {...state, answers: newAnswers }
+    }
+    case 'NEXT':
+      return {
+        ...state,
+        currentQuestion: Math.min(
+          state.currentQuestion + 1,
+          questionsLength - 1
+        )
+      }
+    case 'PREV':
+      return {
+        ...state,
+        currentQuestion: Math.max(state.currentQuestion -1, 0)
+      }
+    case 'SUBMIT':
+      return {
+        ...state,
+        submitted: true
+      }
+    case 'TICK':
+      return {
+        ...state,
+        timeLeft: state.timeLeft -1
+      }
     default:
       return state
   }
